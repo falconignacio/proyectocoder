1e57833f8ae53063985e974602e8686dac87f41c
falconignacio <falconnacho@gmail.com>
2023-08-24 23:44:14 -0300

    entregafinal

---
+++ productos.js
@@ -0,0 +1,62 @@
+const productos = [{
+    id: 1,
+    foto: "./assets/img/saturno.jpg" ,
+    nombre: "Saturno",
+    precio: 6000
+},
+{
+    id: 2,
+    foto: "https://web.whatsapp.com/8324c052-6ba7-48d1-bf07-293fb162b01e",
+    nombre: "Venus",
+    precio: 6300
+},
+{
+    id: 3,
+    foto: "https://web.whatsapp.com/b9c08f0f-81a7-4f4c-9d00-0ae5dcb7bbca",
+    nombre: "Ra",
+    precio: 6750
+},
+{
+    id: 4,
+    foto: "https://web.whatsapp.com/fa167e45-7923-4a64-b2da-65d88a74ecea",
+    nombre: "Neptuno",
+    precio: 5750
+},
+{
+    id: 5,
+    foto: "https://web.whatsapp.com/c287f529-0dc6-4fad-8e1f-4b1ad453d49d",
+    nombre: "Mercurio",
+    precio: 5600
+},
+{
+    id: 6,
+    foto: "https://web.whatsapp.com/38160950-6804-469c-8a63-3c1a92f4af14",
+    nombre: "Afrodita",
+    precio: 5600
+},
+{
+    id: 7,
+    foto: "https://web.whatsapp.com/de432b05-e7dc-4a84-9dad-6068112b3ffc",
+    nombre: "Galaxia",
+    precio: 6750
+},
+{
+    id: 8,
+    foto: "https://web.whatsapp.com/368de418-02cd-4b16-b259-7c9290dee9cd",
+    nombre: "Eclipsis",
+    precio: 6500
+},
+{
+    id: 9,
+    foto: "https://web.whatsapp.com/f621ab9b-c50f-4315-8b41-d41aab5124c7",
+    nombre: "Sirius",
+    precio: 5950
+},
+{
+    id: 10,
+    foto: "https://web.whatsapp.com/42a4950e-f3b0-4780-b8e3-3d9e11523aaa",
+    nombre: "Atenas",
+    precio: 5700
+},
+
+];
